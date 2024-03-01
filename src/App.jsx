160238aa6ae145dfee4cadcf3a3285d1c3fec7a6
DarcilioWeb <darcilioraphael@proton.me>
2024-02-29 21:01:14 -0300

    casa1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PrecioEstudar from './components/PrecioEstudar'
+
+const App = () => {
+  return (
+    <PrecioEstudar NomeDaTecnologia = {"React"} />
+  )
+}
+
+export default App
---
+++ src/components/PrecioEstudar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrecioEstudar = (props) => {
+  return (
+    <div>Preciso estudar {props.NomeDaTecnologia}</div>
+  )
+}
+
+export default PrecioEstudar
